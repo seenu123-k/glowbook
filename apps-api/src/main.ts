8ca20b5d5b3761f41237a67b62fc6053aacded93
Srinivas K <141472650+seenu123-k@users.noreply.github.com>
2026-09-03 16:22:25 +0530

    Bind server to all network interfaces

--- apps-api/src/main.ts
+++ apps-api/src/main.ts
@@ -51,7 +51,7 @@ async function bootstrap() {
   // =========================
   // START SERVER
   // =========================
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 
 bootstrap();
